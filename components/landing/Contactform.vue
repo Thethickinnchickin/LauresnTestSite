f1e3b3a3653197c5241076224555210c74bb1f29
Thethickinnchickin <matt.reiley@sjsu.edu>
2024-05-09 22:32:27 -0700

    commit

--- components/landing/Contactform.vue
+++ components/landing/Contactform.vue
@@ -14,6 +14,7 @@ onMounted(() => {
     const object = Object.fromEntries(formData);
     const json = JSON.stringify(object);
 
+    
     result.innerHTML = "Sending...";
 
     fetch("https://api.web3forms.com/submit", {
